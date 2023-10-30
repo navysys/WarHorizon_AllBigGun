64ffefa33b6e0703775d36cbad0bd3a6b7166024
navysys <51705795+navysys@users.noreply.github.com>
2023-10-30 21:47:09 +0900

    231030-프로젝트생성

---
+++ Source/WarHorizon_AllBigGun/WarHorizon_AllBigGun.Build.cs
@@ -0,0 +1,25 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class WarHorizon_AllBigGun : ModuleRules
+{
+	public WarHorizon_AllBigGun(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicIncludePaths.AddRange(new string[] { "WarHorizon_AllBigGun" });
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/WarHorizon_AllBigGun/WarHorizon_AllBigGun.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WarHorizon_AllBigGun.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WarHorizon_AllBigGun, "WarHorizon_AllBigGun" );
